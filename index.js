063e6708782ec3c1cf54ff4ccf2f52a3cffc1558
Ki Ki  Bush <donyabush@gmail.com>
2018-07-30 00:51:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 // Write your code in this file!
 
-const currentUser = 'Grace Hopper';
+const currentUser = 'Grace Hopper'[0];
 const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
 const excitedWelcomeMessage = welcomeMessage.toUpperCase();
-const shortGreeting = `Welcome, ${currentUser[0]`;
+const shortGreeting = `Welcome, ${currentUser}`;
 
 
 
