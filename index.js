4c7d61298d3d84dfbd723b8d6f94ea06d479898b
Ki Ki  Bush <donyabush@gmail.com>
2018-07-30 00:45:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const currentUser = 'Grace Hopper';
 const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
 const excitedWelcomeMessage = welcomeMessage.toUpperCase();
 const shortGreeting = `Welcome $(currentUser)`;
-const shortGreeting = 'Welcome, ';
+
 
 
 
